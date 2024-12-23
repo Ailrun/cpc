f7eabde0995c1e06953f032d464b1f68a8b57ad8
Junyoung/Clare Jang <jjc9310@gmail.com>
2024-12-23 04:11:37 -0500

    Add basic binary

--- src/bin/cpc.rs
+++ src/bin/cpc.rs
@@ -1,3 +1,32 @@
+use std::{collections::HashMap, io::*, process::exit};
+
+use cpc::{kernel::typecheck::*, parser, syntax::*};
+
 fn main() {
-    println!("Not Yet Implemented...")
+    let mut s = String::new();
+    print!(">> ");
+    stdout().flush().expect("What...?");
+    stdin()
+        .read_to_string(&mut s)
+        .expect("Please input UTF-8 code");
+    if s.is_empty() {
+        println!("\nExit");
+        exit(1)
+    }
+    match parser::full_expression(s.as_str()) {
+        Ok(exp) => {
+            println!("Parsed Expression: {exp:?}");
+            let ctx = HashMap::new();
+            match infer(exp.clone(), &ctx) {
+                Ok(typ) => {
+                    println!("Infered Type: {typ:?}");
+                    let normal_form = exp.clone().nbe(Exp::from(typ), &ctx);
+                    println!("Normalized Expression: {normal_form:?}");
+                }
+                Err(_) => println!("Ill-typed Expression"),
+            }
+        }
+        Err(_) => println!("Parse Error..."),
+    }
+    main()
 }
