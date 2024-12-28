54a5473021fc55ea196bf0297e3fc0ebeb26adea
Junyoung/Clare Jang <jjc9310@gmail.com>
2024-12-28 03:32:53 -0500

    Add one more test

--- tests/typecheck.rs
+++ tests/typecheck.rs
@@ -5,6 +5,14 @@ use std::collections::HashMap;
 use cpc::{kernel::typecheck::*, parser};
 
 #[test]
+fn example_type_check0() {
+    let exp = assert_ok!(parser::full_expression("(fun (a : Univ@0) -> a) Bottom"));
+    let ctx = HashMap::new();
+    let typ = assert_ok!(parser::full_expression("Univ@0")).nbe_typ(&ctx);
+    assert_ok!(check(exp, typ, &ctx));
+}
+
+#[test]
 fn example_type_check1() {
     let exp = assert_ok!(parser::full_expression("(fun (a : Pi (qwe : Univ@1) . Univ@1) -> a) (lambda (b : Univ@1) -> b)"));
     let ctx = HashMap::new();
