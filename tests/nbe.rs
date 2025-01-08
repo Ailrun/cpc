eec273f4b4f99ed485b47fdb177906701bc4b90b
Junyoung/"Clare" Jang <jjc9310@gmail.com>
2025-01-08 06:28:27 -0500

    Mkae tests more informative

--- tests/nbe.rs
+++ tests/nbe.rs
@@ -8,7 +8,12 @@ use cpc::front::parser;
 fn example_nbe0() {
     let exp = assert_ok!(parser::full_expression("(fun (a : Univ@0) -> a) Bottom"));
     let typ = assert_ok!(parser::full_expression("Univ@0"));
-    insta::assert_ron_snapshot!(exp.nbe(typ, &HashMap::new()), @"Bottom");
+    insta::with_settings!({
+      info => &(&exp, &typ),
+      omit_expression => true,
+    }, {
+      insta::assert_ron_snapshot!(exp.nbe(typ, &HashMap::new()), @"Bottom");
+    });
 }
 
 #[test]
@@ -17,13 +22,18 @@ fn example_nbe1() {
         "(fun (a : Pi (qwe : Univ@1) . Univ@1) -> a) (lambda (b : Univ@1) -> b)"
     ));
     let typ = assert_ok!(parser::full_expression("Pi (d : Univ@1) . Univ@1"));
-    insta::assert_ron_snapshot!(exp.nbe(typ, &HashMap::new()), @r#"
-    Fun(Fun(
-      param: TypedName(
-        name: "d",
-        typ: Univ(1),
-      ),
-      body: Neut(Var("d")),
-    ))
-    "#);
+    insta::with_settings!({
+      info => &(&exp, &typ),
+      omit_expression => true,
+    }, {
+      insta::assert_ron_snapshot!(exp.nbe(typ, &HashMap::new()), @r#"
+      Fun(Fun(
+        param: TypedName(
+          name: "d",
+          typ: Univ(1),
+        ),
+        body: Neut(Var("d")),
+      ))
+      "#);
+    });
 }
