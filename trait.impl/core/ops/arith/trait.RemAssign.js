c6ae1097db3fb93268c84e0a5f56d9f5ddbc84c5
Ailrun <jjc9310@gmail.com>
2025-01-23 04:37:13 +0000

    Deploying to gh-pages from @ Ailrun/cpc@ad871a9e6454cec372e79d609b837ddd7e453a88 🚀

--- trait.impl/core/ops/arith/trait.RemAssign.js
+++ trait.impl/core/ops/arith/trait.RemAssign.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["adler2",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/arith/trait.RemAssign.html\" title=\"trait core::ops::arith::RemAssign\">RemAssign</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.84.0/core/primitive.u32.html\">u32</a>&gt; for <a class=\"struct\" href=\"adler2/algo/struct.U32X4.html\" title=\"struct adler2::algo::U32X4\">U32X4</a>"]]],["serde",[]]]);
+    var implementors = Object.fromEntries([["adler2",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/ops/arith/trait.RemAssign.html\" title=\"trait core::ops::arith::RemAssign\">RemAssign</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.83.0/core/primitive.u32.html\">u32</a>&gt; for <a class=\"struct\" href=\"adler2/algo/struct.U32X4.html\" title=\"struct adler2::algo::U32X4\">U32X4</a>"]]],["serde",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/nom/error/trait.ContextError.js
+++ trait.impl/nom/error/trait.ContextError.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["nom",[]],["nom_supreme",[["impl&lt;I, T, E&gt; <a class=\"trait\" href=\"nom/error/trait.ContextError.html\" title=\"trait nom::error::ContextError\">ContextError</a>&lt;I&gt; for <a class=\"enum\" href=\"nom_supreme/error/enum.GenericErrorTree.html\" title=\"enum nom_supreme::error::GenericErrorTree\">GenericErrorTree</a>&lt;I, T, &amp;'static <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.84.0/std/primitive.str.html\">str</a>, E&gt;"]]]]);
+    var implementors = Object.fromEntries([["nom",[]],["nom_supreme",[["impl&lt;I, T, E&gt; <a class=\"trait\" href=\"nom/error/trait.ContextError.html\" title=\"trait nom::error::ContextError\">ContextError</a>&lt;I&gt; for <a class=\"enum\" href=\"nom_supreme/error/enum.GenericErrorTree.html\" title=\"enum nom_supreme::error::GenericErrorTree\">GenericErrorTree</a>&lt;I, T, &amp;'static <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.83.0/std/primitive.str.html\">str</a>, E&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
