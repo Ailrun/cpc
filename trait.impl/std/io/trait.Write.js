07fb18ba567537d20cfcdd82d0146b049593b257
Ailrun <jjc9310@gmail.com>
2025-02-02 11:03:01 +0000

    Deploying to gh-pages from @ Ailrun/cpc@20aa3e38bf8ee89a7acb049d63f60cada9d63f9f 🚀

--- trait.impl/std/io/trait.Write.js
+++ trait.impl/std/io/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["indent_write",[["impl&lt;'i, W: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"indent_write/io/struct.IndentWriter.html\" title=\"struct indent_write::io::IndentWriter\">IndentWriter</a>&lt;'i, W&gt;"]]],["serde",[]]]);
+    var implementors = Object.fromEntries([["indent_write",[["impl&lt;'i, W: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"indent_write/io/struct.IndentWriter.html\" title=\"struct indent_write::io::IndentWriter\">IndentWriter</a>&lt;'i, W&gt;"]]],["serde",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
