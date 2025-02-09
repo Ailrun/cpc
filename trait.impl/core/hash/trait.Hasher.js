1d1d8de58063b3d7c5cf9b9e8a4b9fc3dd2a0505
Ailrun <jjc9310@gmail.com>
2025-02-09 20:25:29 +0000

    Deploying to gh-pages from @ Ailrun/cpc@9df7ec53f46d0dc30f77445f4fc77b3666908a8d 🚀

--- trait.impl/core/hash/trait.Hasher.js
+++ trait.impl/core/hash/trait.Hasher.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["adler2",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"adler2/struct.Adler32.html\" title=\"struct adler2::Adler32\">Adler32</a>"]]],["fnv",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"fnv/struct.FnvHasher.html\" title=\"struct fnv::FnvHasher\">FnvHasher</a>"]]],["libc",[]],["serde",[]]]);
+    var implementors = Object.fromEntries([["adler2",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"adler2/struct.Adler32.html\" title=\"struct adler2::Adler32\">Adler32</a>"]]],["fnv",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"fnv/struct.FnvHasher.html\" title=\"struct fnv::FnvHasher\">FnvHasher</a>"]]],["libc",[]],["serde",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
